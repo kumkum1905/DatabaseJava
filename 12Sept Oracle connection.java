02bc8781274194ef1a82cc8a2097c5e6baad4eae
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2025-09-12 10:41:22 +0530

    Create 12Sept Oracle connection.java

---
+++ 12Sept Oracle connection.java
@@ -0,0 +1,31 @@
+/*
+1. open orcale 10g and login(username: system and password: system )
+	DataBase Name is XE
+
+2. Oracle(XE) is ready now connect it with java
+use orcale connector in "....jdbc/lib/filename"
+
+we need to set the classpath : system env
+use: ;.; in the end 
+
+3. start-> edit sys var -> Env Var -> New -> (Fill the field according [Name , Path])
+Path: C:\oraclexe\app\oracle\product\10.2.0\server\jdbc\lib/(filename with extension)
+
+4. Write code of java show below
+*/
+
+import java.sql.* ;
+
+class DB{
+	public static void main(String[] args) throws ClassNotFoundException, SQLException{
+		//Load Driver Class
+		Class.forName("oracle.jdbc.Driver.OracleDriver");
+		//Create connection
+		Connection con = DriverManager.getConnection("jdbc:oracle:this:@localhost:1521:XE","system","system");
+
+		System.out.println(con);
+
+		//close connection
+		con.close();
+	}
+}
