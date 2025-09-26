7ab944659387d16070d2fb3ad7dd33bcbccae5e3
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2025-09-26 09:51:30 +0530

    Update 12Sept Oracle connection.java

--- 12Sept Oracle connection.java
+++ 12Sept Oracle connection.java
@@ -19,7 +19,7 @@ import java.sql.* ;
 class DB{
 	public static void main(String[] args) throws ClassNotFoundException, SQLException{
 		//Load Driver Class
-		Class.forName("oracle.jdbc.Driver.OracleDriver");
+		Class.forName("oracle.jdbc.driver.OracleDriver");
 		//Create connection
 		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE","system","system");
 
